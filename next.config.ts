0e825689ca5b25974c8e3a31879397b4c0aa0a5c
ADam-BGZ <adam.mohammed.belghazi@gmail.com>
2026-07-28 22:55:21 +0100

    fix: update base path and workflow for new repo cabinet_dentaire_dr_boutaina_bouhaouita

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/cabinet_dentaire_dr_zahir_nabil" : "",
-  assetPrefix: isProd ? "/cabinet_dentaire_dr_zahir_nabil/" : "",
+  basePath: isProd ? "/cabinet_dentaire_dr_boutaina_bouhaouita" : "",
+  assetPrefix: isProd ? "/cabinet_dentaire_dr_boutaina_bouhaouita/" : "",
   images: {
     unoptimized: true,
   },
